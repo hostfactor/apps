54e4d69b2ab5e352b42ad6c1e3c22cf3e772a063
edwardowens <eddiebeme@gmail.com>
2023-12-01 12:22:27 -0800

    feat(tmod): Adding workflow dispatch to github action. Removing tmod version arg default.

--- manibuild/github.go
+++ manibuild/github.go
@@ -1,104 +1,109 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 type GithubAction struct {
-  Name string                     `json:"name,omitempty" yaml:"name,omitempty"`
-  On   *GithubActionTrigger       `json:"on,omitempty" yaml:"on,omitempty"`
-  Jobs map[string]GithubActionJob `json:"jobs,omitempty" yaml:"jobs,omitempty"`
-  Env  map[string]string          `json:"env,omitempty" yaml:"env,omitempty"`
+	Name string                     `json:"name,omitempty" yaml:"name,omitempty"`
+	On   *GithubActionTrigger       `json:"on,omitempty" yaml:"on,omitempty"`
+	Jobs map[string]GithubActionJob `json:"jobs,omitempty" yaml:"jobs,omitempty"`
+	Env  map[string]string          `json:"env,omitempty" yaml:"env,omitempty"`
 }
 
 type GithubActionJob struct {
-  Name   string                `json:"name,omitempty" yaml:"name,omitempty"`
-  Uses   string                `json:"uses,omitempty" yaml:"uses,omitempty"`
-  With   map[string]string     `json:"with,omitempty" yaml:"with,omitempty"`
-  RunsOn string                `json:"runs-on,omitempty" yaml:"runs-on,omitempty"`
-  Steps  []GithubActionJobStep `json:"steps,omitempty" yaml:"steps,omitempty"`
+	Name   string                `json:"name,omitempty" yaml:"name,omitempty"`
+	Uses   string                `json:"uses,omitempty" yaml:"uses,omitempty"`
+	With   map[string]string     `json:"with,omitempty" yaml:"with,omitempty"`
+	RunsOn string                `json:"runs-on,omitempty" yaml:"runs-on,omitempty"`
+	Steps  []GithubActionJobStep `json:"steps,omitempty" yaml:"steps,omitempty"`
 }
 
 type GithubActionJobStep struct {
-  Uses string            `json:"uses,omitempty" yaml:"uses,omitempty"`
-  Run  string            `json:"run,omitempty" yaml:"run,omitempty"`
-  Env  map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
-  Name string            `json:"name,omitempty" yaml:"name,omitempty"`
-  With map[string]string `json:"with,omitempty" yaml:"with,omitempty"`
-  Id   string            `json:"id,omitempty" yaml:"id,omitempty"`
+	Uses string            `json:"uses,omitempty" yaml:"uses,omitempty"`
+	Run  string            `json:"run,omitempty" yaml:"run,omitempty"`
+	Env  map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+	Name string            `json:"name,omitempty" yaml:"name,omitempty"`
+	With map[string]string `json:"with,omitempty" yaml:"with,omitempty"`
+	Id   string            `json:"id,omitempty" yaml:"id,omitempty"`
 }
 
 type GithubActionTrigger struct {
-  Schedule []GithubActionScheduleCron `json:"schedule,omitempty" yaml:"schedule,omitempty"`
-  Push     *GithubActionPush          `json:"push,omitempty" yaml:"push,omitempty"`
+	Schedule         []GithubActionScheduleCron    `json:"schedule,omitempty" yaml:"schedule,omitempty"`
+	Push             *GithubActionPush             `json:"push,omitempty" yaml:"push,omitempty"`
+	WorkflowDispatch *GithubActionWorkflowDispatch `json:"workflow_dispatch,omitempty" yaml:"workflow_dispatch,omitempty"`
+}
+
+type GithubActionWorkflowDispatch struct {
+	Inputs map[string]string `json:"inputs,omitempty" yaml:"inputs,omitempty"`
 }
 
 type GithubActionPush struct {
-  Branches []string `json:"branches,omitempty" yaml:"branches,omitempty"`
-  Paths    []string `json:"paths,omitempty" yaml:"paths,omitempty"`
+	Branches []string `json:"branches,omitempty" yaml:"branches,omitempty"`
+	Paths    []string `json:"paths,omitempty" yaml:"paths,omitempty"`
 }
 
 type GithubActionScheduleCron struct {
-  Cron string `json:"cron,omitempty" yaml:"cron,omitempty"`
+	Cron string `json:"cron,omitempty" yaml:"cron,omitempty"`
 }
 
 var CheckoutGithubAction = GithubActionJobStep{
-  Uses: "actions/checkout@v4",
-  Name: "Checkout",
+	Uses: "actions/checkout@v4",
+	Name: "Checkout",
 }
 
 type BuildImageGithubActionSpec struct {
-  Name             string
-  File             string
-  Context          string
-  Tags             []string
-  ImageDescription string
-  BuildArgs        map[string]string
+	Name             string
+	File             string
+	Context          string
+	Tags             []string
+	ImageDescription string
+	BuildArgs        map[string]string
 }
 
 func BuildImageGithubAction(spec BuildImageGithubActionSpec) GithubActionJobStep {
-  out := GithubActionJobStep{
-    Uses: "./.github/actions/build_image",
-    Name: "Build and deploy image",
-    With: map[string]string{},
-  }
+	out := GithubActionJobStep{
+		Uses: "./.github/actions/build_image",
+		Name: "Build and deploy image",
+		With: map[string]string{},
+	}
 
-  out.With["image_description"] = spec.ImageDescription
-  out.With["name"] = spec.Name
-  out.With["file"] = spec.File
+	out.With["image_description"] = spec.ImageDescription
+	out.With["name"] = spec.Name
+	out.With["file"] = spec.File
 
-  if spec.Context != "" {
-    out.With["context"] = spec.Context
-  }
+	if spec.Context != "" {
+		out.With["context"] = spec.Context
+	}
 
-  if len(spec.Tags) > 0 {
-    out.With["tags"] = strings.Join(spec.Tags, "\n")
-  }
+	if len(spec.Tags) > 0 {
+		out.With["tags"] = strings.Join(spec.Tags, "\n")
+	}
 
-  if len(spec.BuildArgs) > 0 {
-    ba := make([]string, 0, len(spec.BuildArgs))
-    for k, v := range spec.BuildArgs {
-      ba = append(ba, fmt.Sprintf("%s=%s", k, v))
-    }
-    out.With["build_args"] = strings.Join(ba, "\n")
-  }
+	if len(spec.BuildArgs) > 0 {
+		ba := make([]string, 0, len(spec.BuildArgs))
+		for k, v := range spec.BuildArgs {
+			ba = append(ba, fmt.Sprintf("%s=%s", k, v))
+		}
+		out.With["build_args"] = strings.Join(ba, "\n")
+	}
 
-  return out
+	return out
 }
 
 type GetLatestGithubReleaseActionOutputs struct {
-  TagName string
+	TagName string
 }
 
 func GetLatestGithubReleaseAction(repo string) (GithubActionJobStep, GetLatestGithubReleaseActionOutputs) {
-  out := GithubActionJobStep{
-    Uses: "./.github/actions/get_github_release",
-    Name: "Get Github release",
-    Id:   "github_release",
-    With: map[string]string{
-      "repo": repo,
-    },
-  }
-  return out, GetLatestGithubReleaseActionOutputs{TagName: fmt.Sprintf("steps.%s.outputs.tag_name", out.Id)}
+	out := GithubActionJobStep{
+		Uses: "./.github/actions/get_github_release",
+		Name: "Get Github release",
+		Id:   "github_release",
+		With: map[string]string{
+			"repo": repo,
+		},
+	}
+	return out, GetLatestGithubReleaseActionOutputs{TagName: fmt.Sprintf("steps.%s.outputs.tag_name", out.Id)}
 }
--- manibuild/manifest.go
+++ manibuild/manifest.go
@@ -1,141 +1,142 @@
 package main
 
 import (
-  "errors"
-  "fmt"
-  "gopkg.in/yaml.v3"
-  "io/fs"
-  "os"
-  "path/filepath"
+	"errors"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io/fs"
+	"os"
+	"path/filepath"
 )
 
 var manifestDir fs.FS
 
 func GenerateGithubActions(manifestFile, targetFolder string) error {
-  dir, manifestFilename := filepath.Split(manifestFile)
-  if manifestDir == nil {
-    manifestDir = os.DirFS(dir)
-  }
+	dir, manifestFilename := filepath.Split(manifestFile)
+	if manifestDir == nil {
+		manifestDir = os.DirFS(dir)
+	}
 
-  b, err := fs.ReadFile(manifestDir, manifestFilename)
-  if err != nil {
-    return err
-  }
+	b, err := fs.ReadFile(manifestDir, manifestFilename)
+	if err != nil {
+		return err
+	}
 
-  mani := new(Manifest)
-  err = yaml.Unmarshal(b, mani)
-  if err != nil {
-    return errors.Join(fmt.Errorf("failed to read manifest %s", manifestFile), err)
-  }
+	mani := new(Manifest)
+	err = yaml.Unmarshal(b, mani)
+	if err != nil {
+		return errors.Join(fmt.Errorf("failed to read manifest %s", manifestFile), err)
+	}
 
-  for _, v := range mani.Features {
-    act, err := FeatureToGithubAction(mani.Name, manifestFile, &v)
-    if err != nil {
-      return errors.Join(fmt.Errorf("failed to generate action for %s", v.Name), err)
-    }
+	for _, v := range mani.Features {
+		act, err := FeatureToGithubAction(mani.Name, manifestFile, &v)
+		if err != nil {
+			return errors.Join(fmt.Errorf("failed to generate action for %s", v.Name), err)
+		}
 
-    fileName := fmt.Sprintf("%s_%s.yml", mani.Name, v.Name)
-    fp := filepath.Join(targetFolder, fileName)
-    err = writeGithubActionFile(fp, &v, act)
-    if err != nil {
-      return err
-    }
-  }
+		fileName := fmt.Sprintf("%s_%s.yml", mani.Name, v.Name)
+		fp := filepath.Join(targetFolder, fileName)
+		err = writeGithubActionFile(fp, &v, act)
+		if err != nil {
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func writeGithubActionFile(fp string, feat *Feature, act *GithubAction) error {
-  f, err := os.OpenFile(fp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
-  if err != nil {
-    return errors.Join(fmt.Errorf("failed to write file %s", fp), err)
-  }
-  defer f.Close()
-  enc := yaml.NewEncoder(f)
-  enc.SetIndent(2)
-  fmt.Println("Writing Github action for feature", feat.Name, "to", fp)
-  err = enc.Encode(act)
-  if err != nil {
-    return errors.Join(fmt.Errorf("failed to write file %s", fp), err)
-  }
-  return nil
+	f, err := os.OpenFile(fp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return errors.Join(fmt.Errorf("failed to write file %s", fp), err)
+	}
+	defer f.Close()
+	enc := yaml.NewEncoder(f)
+	enc.SetIndent(2)
+	fmt.Println("Writing Github action for feature", feat.Name, "to", fp)
+	err = enc.Encode(act)
+	if err != nil {
+		return errors.Join(fmt.Errorf("failed to write file %s", fp), err)
+	}
+	return nil
 }
 
 type Manifest struct {
-  Name        string    `yaml:"name,omitempty" json:"name,omitempty"`
-  Author      string    `yaml:"author,omitempty" json:"author,omitempty"`
-  Description string    `yaml:"description,omitempty" json:"description,omitempty"`
-  Features    []Feature `yaml:"features,omitempty" json:"features,omitempty"`
+	Name        string    `yaml:"name,omitempty" json:"name,omitempty"`
+	Author      string    `yaml:"author,omitempty" json:"author,omitempty"`
+	Description string    `yaml:"description,omitempty" json:"description,omitempty"`
+	Features    []Feature `yaml:"features,omitempty" json:"features,omitempty"`
 }
 
 type Feature struct {
-  Name        string            `yaml:"name,omitempty" json:"name,omitempty"`
-  Tags        []string          `yaml:"tags,omitempty" json:"tags,omitempty"`
-  Context     string            `yaml:"context,omitempty" json:"context,omitempty"`
-  Watch       *FeatureWatch     `yaml:"watch,omitempty" json:"watch,omitempty"`
-  BuildArgs   map[string]string `yaml:"build_args,omitempty" json:"build_args,omitempty"`
-  Description string            `yaml:"description" json:"description"`
+	Name        string            `yaml:"name,omitempty" json:"name,omitempty"`
+	Tags        []string          `yaml:"tags,omitempty" json:"tags,omitempty"`
+	Context     string            `yaml:"context,omitempty" json:"context,omitempty"`
+	Watch       *FeatureWatch     `yaml:"watch,omitempty" json:"watch,omitempty"`
+	BuildArgs   map[string]string `yaml:"build_args,omitempty" json:"build_args,omitempty"`
+	Description string            `yaml:"description" json:"description"`
 }
 
 type FeatureWatch struct {
-  GithubRelease *GithubReleaseFeatureWatch `yaml:"github_release,omitempty" json:"github_release,omitempty"`
+	GithubRelease *GithubReleaseFeatureWatch `yaml:"github_release,omitempty" json:"github_release,omitempty"`
 }
 
 type GithubReleaseFeatureWatch struct {
-  Repo string `yaml:"repo,omitempty" json:"repo,omitempty"`
+	Repo string `yaml:"repo,omitempty" json:"repo,omitempty"`
 }
 
 func FeatureToGithubAction(appName, manifestFilePath string, f *Feature) (*GithubAction, error) {
-  dir := filepath.Dir(manifestFilePath)
-  jobName := fmt.Sprintf("build_and_deploy_%s_%s", appName, f.Name)
+	dir := filepath.Dir(manifestFilePath)
+	jobName := fmt.Sprintf("build_and_deploy_%s_%s", appName, f.Name)
 
-  job := GithubActionJob{
-    Name: fmt.Sprintf("Build and deploy %s: %s", appName, f.Name),
-    Steps: []GithubActionJobStep{
-      CheckoutGithubAction,
-    },
-    RunsOn: "ubuntu-latest",
-  }
+	job := GithubActionJob{
+		Name: fmt.Sprintf("Build and deploy %s: %s", appName, f.Name),
+		Steps: []GithubActionJobStep{
+			CheckoutGithubAction,
+		},
+		RunsOn: "ubuntu-latest",
+	}
 
-  act := &GithubAction{
-    Name: f.Name,
-    On: &GithubActionTrigger{
-      Push: &GithubActionPush{
-        Branches: []string{
-          "master",
-        },
-        Paths: []string{
-          filepath.Join(dir, f.Context, "**"),
-          filepath.Join(dir, "app.sha"),
-        },
-      },
-    },
-    Jobs: map[string]GithubActionJob{},
-  }
+	act := &GithubAction{
+		Name: f.Name,
+		On: &GithubActionTrigger{
+			WorkflowDispatch: &GithubActionWorkflowDispatch{},
+			Push: &GithubActionPush{
+				Branches: []string{
+					"master",
+				},
+				Paths: []string{
+					filepath.Join(dir, f.Context, "**"),
+					filepath.Join(dir, "app.sha"),
+				},
+			},
+		},
+		Jobs: map[string]GithubActionJob{},
+	}
 
-  buildImageStep := BuildImageGithubAction(BuildImageGithubActionSpec{
-    Name:             appName,
-    Context:          filepath.Join(dir, f.Context),
-    Tags:             f.Tags,
-    ImageDescription: f.Description,
-    BuildArgs:        f.BuildArgs,
-  })
+	buildImageStep := BuildImageGithubAction(BuildImageGithubActionSpec{
+		Name:             appName,
+		Context:          filepath.Join(dir, f.Context),
+		Tags:             f.Tags,
+		ImageDescription: f.Description,
+		BuildArgs:        f.BuildArgs,
+	})
 
-  if f.Watch != nil {
-    act.On.Schedule = []GithubActionScheduleCron{
-      {
-        Cron: "0 */12 * * *",
-      },
-    }
-    if f.Watch.GithubRelease != nil {
-      st, _ := GetLatestGithubReleaseAction(f.Watch.GithubRelease.Repo)
-      job.Steps = append(job.Steps, st)
-    }
-  }
+	if f.Watch != nil {
+		act.On.Schedule = []GithubActionScheduleCron{
+			{
+				Cron: "0 */12 * * *",
+			},
+		}
+		if f.Watch.GithubRelease != nil {
+			st, _ := GetLatestGithubReleaseAction(f.Watch.GithubRelease.Repo)
+			job.Steps = append(job.Steps, st)
+		}
+	}
 
-  job.Steps = append(job.Steps, buildImageStep)
+	job.Steps = append(job.Steps, buildImageStep)
 
-  act.Jobs[jobName] = job
+	act.Jobs[jobName] = job
 
-  return act, nil
+	return act, nil
 }
